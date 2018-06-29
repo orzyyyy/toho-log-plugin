8c37eb5725b552a7f911bd9ee233793c2fb092db
zy410419243 <zy410419243@gmail.com>
2018-06-29 16:39:26 +0800

    update

--- log.js
+++ log.js
@@ -2,7 +2,7 @@
  * @Author: zy9@github.com/zy410419243 
  * @Date: 2018-06-18 13:43:52 
  * @Last Modified by: zy9
- * @Last Modified time: 2018-06-19 21:31:41
+ * @Last Modified time: 2018-06-29 16:39:22
  */
 const chalk = require('chalk');
 
@@ -31,7 +31,7 @@ const logInfo = (err, stats, dev) => {
             error(item);
         }
 
-        error('\n  少女以为能神穿，但缠在腰间的香火钱太多，戳之，卒 (°□°；) \n');
+        error('\n  少女以为能神穿，奈何缠在腰间的香火钱太多，戳之，卒 (°□°；) \n');
 
         return;
     }
