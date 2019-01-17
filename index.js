be870803b66c4100733d09f548f72fc31f2a56fa
zy410419243 <zy410419243@gmail.com>
2019-01-17 16:51:08 +0800

    feat: add isPray for webpackbar

--- index.js
+++ index.js
@@ -4,56 +4,62 @@
  * @Last Modified by: zy9
  * @Last Modified time: 2018-12-01 15:10:30
  */
-const { log, error, warn, info, logInfo } = require('./log');
+const { log, error, warn, info, logInfo } = require("./log");
 let words = [];
 
 class TohoLogPlugin {
-	constructor (options) {
-		options = Object.assign({}, { dev: true, defaultWords: false }, options);
+  constructor(options) {
+    options = Object.assign(
+      {},
+      { dev: true, defaultWords: false, isPray: true },
+      options
+    );
 
-		if(options.path === undefined && options.defaultWords) {
-			options.path = './word.json';
-		}
+    if (options.path === undefined && options.defaultWords) {
+      options.path = "./word.json";
+    }
 
-		this.options = options;
-	}
+    this.options = options;
+  }
 
-	apply (compiler) {
-		const { dev } = this.options;
-		const tap = 'log';
+  apply(compiler) {
+    const { dev, isPray } = this.options;
+    const tap = "log";
 
-		const superInfo = () => {
-			info('  少女祈祷中...');
-		};
+    const superInfo = () => {
+      if (isPray) {
+        info("  少女祈祷中...");
+      }
+    };
 
-		compiler.hooks.entryOption.tap(tap, () => {
-			const { path } = this.options;
+    compiler.hooks.entryOption.tap(tap, () => {
+      const { path } = this.options;
 
-			if(path) {
-				try {
-					words = require(path);
-				} catch (err) {
-					error('这里没显示单词大概是路径错了\r\n');
-				}
-			}
-		});
+      if (path) {
+        try {
+          words = require(path);
+        } catch (err) {
+          error("这里没显示单词大概是路径错了\r\n");
+        }
+      }
+    });
 
-		compiler.hooks.watchRun.tap(tap, () => {
-			superInfo();
-		});
+    compiler.hooks.watchRun.tap(tap, () => {
+      superInfo();
+    });
 
-		compiler.hooks.run.tap(tap, () => {
-			superInfo();
-		});
+    compiler.hooks.run.tap(tap, () => {
+      superInfo();
+    });
 
-		compiler.hooks.failed.tap(tap, err => {
-			logInfo(err, undefined, dev);
-		});
+    compiler.hooks.failed.tap(tap, err => {
+      logInfo(err, undefined, dev);
+    });
 
-		compiler.hooks.done.tap(tap, stats => {
-			logInfo(undefined, stats, dev, words);
-		});
-	}
+    compiler.hooks.done.tap(tap, stats => {
+      logInfo(undefined, stats, dev, words);
+    });
+  }
 }
 
 module.exports = TohoLogPlugin;
